76ae94550406cc51892a26efeb7c8db219bde64b
SeanDu <aijisud@gmail.com>
2016-12-15 00:38:13 +0800

    fix

--- v.py
+++ v.py
@@ -5,7 +5,7 @@ import shutil
 
 url = 'http://weibo.com/u/1302237377?is_all=1'
 req = urllib2.Request(url)
-response = ulrlib2.urlopen(req)
+response = urllib2.urlopen(req)
 html = response.read()
 
 file_object = open('index.html', 'w')
