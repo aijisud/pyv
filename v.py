0648f37d5eae67f93c87c3ca4afb46f73e11d143
SeanDu <aijisud@gmail.com>
2016-12-15 02:06:41 +0800

    site test

--- v.py
+++ v.py
@@ -3,11 +3,11 @@ import os.path
 import shutil
 
 
-url = 'http://weibo.com/u/1302237377?is_all=1'
+url = 'http://baike.baidu.com/item/ubuntu'
 req = urllib2.Request(url)
 response = urllib2.urlopen(req)
 html = response.read()
 
-file_object = open('index.html', 'w')
+file_object = open('v/index.html', 'w')
 file_object.write(html)
 file_object.close()
