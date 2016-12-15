115c0958ea81155050a6b6e0a1e3b29fd45d6449
SeanDu <aijisud@gmail.com>
2016-12-15 19:29:07 +0800

    noutf-8char

--- v.py
+++ v.py
@@ -15,21 +15,13 @@ def GetWBDetail():
     response = urllib2.urlopen(request)
     pagecontent = response.read()
 
-    #encode的作用是将unicode编码转换成其他编码的字符串
-    #decode的作用是将其他编码的字符串转换成unicode编码
-    #unicodecontent = pagecontent.decode("utf-8")
+    unicodecontent = pagecontent.decode("utf-8")
 
-    #找出所有class="WB_detail"的div标记
-    #re.S是任意匹配模式，也就是.可以匹配换行符
-    rawItems = re.findall('<div class="WB_detail">.*?<div class="WB_from S_txt2">(.*?)</div>.*?<div class="WB_text W_f14" node-type="feed_list_content">(.*?)</div>',pagecontent,re.S)
+    rawItems = re.findall('<div class="WB_detail">.*?<div class="WB_from S_txt2">(.*?)</div>.*?<div class="WB_text W_f14" node-type="feed_list_content">(.*?)</div>',unicodecontent,re.S)
     items = []
     for item in rawItems:
-        # item 中第一个是div的标题，也就是时间
-        # item 中第二个是div的内容，也就是内容
         items.append([item[0],item[1])
     return items
-
-
 
 
 htmlItems = GetWBDetail()
